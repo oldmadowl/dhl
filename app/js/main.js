7549bcd945875f709abe6d9ce7bc67783c34b77d
oldmadowl <oldmadowl@gmail.com>
2022-05-26 16:19:20 +0300

    added header

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,13 @@
 $(function(){
 
-  
+  $('select').styler();
+
+  $('.header__search').on('click', function(){
+    $('.header__search-form').toggleClass('active');
+  });
+
+  $('.menu__btn').on('click', function(){
+    $('.menu__list').slideToggle();
+  });
 
 });
